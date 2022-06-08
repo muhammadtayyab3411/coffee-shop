7f4677defa491edea2ffb951a6cd72e617eb92cb
Muhammad Tayyab <muhammadtayyab3411@gmail.com>
2022-06-08 12:05:03 -0700

    DB setup

--- src/db/DB_connection.js
+++ src/db/DB_connection.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(`${process.env.DB_URL}`, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
     console.log("Connection Successful");
 })
