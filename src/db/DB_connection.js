0d887eeb53a0ffde9fb3a3346a6522de59dfa5b6
Muhammad Tayyab <muhammadtayyab3411@gmail.com>
2022-06-09 10:41:15 -0700

    DB testing

--- src/db/DB_connection.js
+++ src/db/DB_connection.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://muhammadtayyab3411:Tayyab3411.@cluster0.koaqv.mongodb.net/coffee-shop-db?retryWrites=true", {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
     console.log("Connection Successful");
 })
